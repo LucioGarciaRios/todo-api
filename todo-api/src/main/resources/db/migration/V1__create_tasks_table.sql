5587c286803657c21e3d5480c73e39f42b8c36a6
Lucio-Garcia-Rios <lucio_rios@goodyear.com>
2025-02-18 22:39:27 -0300

    fix project structure

--- todo-api/src/main/resources/db/migration/V1__create_tasks_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tasks (
-                       id INT PRIMARY KEY IDENTITY(1,1),
-                       title NVARCHAR(255) NOT NULL,
-                       description NVARCHAR(500),
-                       created_at DATETIME DEFAULT GETDATE(),
-                       status NVARCHAR(50) CHECK (status IN ('pendente', 'em andamento', 'concluída'))
-);
